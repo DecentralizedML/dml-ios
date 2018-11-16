d4d702a488c5cd5a5347b61ee18c55e7c8b0273c
Aaron Cruz <pferdefleisch+git@gmail.com>
2018-11-16 15:08:24 +0100

    Initial Commit

---
+++ DMLMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DMLMobile
+//
+//  Created by Aaron Cruz on 16/11/2018.
+//  Copyright © 2018 Square Bracket eU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
